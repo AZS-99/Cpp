2e4ab5a84119dbe64e954ccc2a98c92a6d4f0404
Adam Saher <adamsaher@Adams-MacBook-Pro.local>
2019-01-16 00:28:11 -0500

    Adding Array project

---
+++ Array/Array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Array
+//
+//  Created by Adam Saher on 16/01/2019.
+//  Copyright © 2019 Adam Saher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
