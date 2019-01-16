f0b991972fad3a9eaff7bc6b1da6ea02c4bd04c3
Adam Saher <adamsaher@Adams-MacBook-Pro.local>
2019-01-16 00:31:49 -0500

    Adding array.h

--- Array/Array/main.cpp
+++ Array/Array/main.cpp
@@ -2,14 +2,23 @@
 //  main.cpp
 //  Array
 //
-//  Created by Adam Saher on 16/01/2019.
-//  Copyright © 2019 Adam Saher. All rights reserved.
+//  Created by Adam Saher on 17/12/2018.
+//  Copyright © 2018 Adam Saher. All rights reserved.
 //
 
 #include <iostream>
+#include <array>
+
+#include "Array.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Container::Array<int, 4> a = {1, 3, 4};
+    std::cout << a << std::endl;
+    Container::Array<int, 4> b;
+    
+    std::array<size_t, 5> c = {{1}};
+    for (auto& element : c)
+        std::cout << element;
     return 0;
 }
+
