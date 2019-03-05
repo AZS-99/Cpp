6878fc7fe348d8deb70f230e9bb71e0f44f5ecb8
AZS-99 <saher.zachary.adam@icloud.com>
2019-03-05 13:49:37 -0500

    End of hash main

--- HashTable/HashTable/main.cpp
+++ HashTable/HashTable/main.cpp
@@ -9,7 +9,9 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::hash<std::string> hash_fn_ptr;
+    std::string a("hello");
+    size_t hash_value = hash_fn_ptr(a);
+    std::cout << hash_value << std::endl;
     return 0;
 }
