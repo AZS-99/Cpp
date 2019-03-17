151e3284ecdc5f7e82be98915cd3f15045ee4e6e
AZS-99 <saher.zachary.adam@icloud.com>
2019-03-17 18:52:13 -0400

    removing

--- String/Build/Intermediates.noindex/String.build/Debug/String.build/Objects-normal/x86_64/String.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/adamsaher/Library/Mobile\ Documents/com~apple~CloudDocs/Drive/Courses/C++/Cpp/String/String/String.cpp \
-  /Users/adamsaher/Library/Mobile\ Documents/com~apple~CloudDocs/Drive/Courses/C++/Cpp/String/String/String.hpp
--- String/Build/Intermediates.noindex/String.build/Debug/String.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/adamsaher/Library/Mobile\ Documents/com~apple~CloudDocs/Drive/Courses/C++/Cpp/String/String/main.cpp \
-  /Users/adamsaher/Library/Mobile\ Documents/com~apple~CloudDocs/Drive/Courses/C++/Cpp/String/String/String.hpp
