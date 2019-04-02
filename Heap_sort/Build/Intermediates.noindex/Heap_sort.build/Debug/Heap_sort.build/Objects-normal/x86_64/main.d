76f34605157e530ac258ae4d6081050fc6ebba3e
AZS-99 <saher.zachary.adam@icloud.com>
2019-04-01 22:24:41 -0400

    Moving all sort algorithms to one directory

--- Heap_sort/Build/Intermediates.noindex/Heap_sort.build/Debug/Heap_sort.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/adamsaher/Library/Mobile\ Documents/com~apple~CloudDocs/Drive/Courses/C++/Cpp/Heap_sort/Heap_sort/main.cpp
--- Quick_sort/Build/Intermediates.noindex/Quick_sort.build/Debug/Quick_sort.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/adamsaher/Library/Mobile\ Documents/com~apple~CloudDocs/Drive/Courses/C++/Cpp/Quick_sort/Quick_sort/main.cpp
