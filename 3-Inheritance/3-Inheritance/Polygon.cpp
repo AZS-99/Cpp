782ff89dec3cd715092aef9cbfd7828a7ca94c13
Adam <adam@Adams-MacBook-Pro.local>
2021-03-21 15:25:05 -0400

    Delete Inheritance

--- 3-Inheritance/3-Inheritance/Polygon.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  Polygon.cpp
-//  3-Inheritance
-//
-//  Created by Adam Saher on 2021-03-14.
-//
-
-#include "Polygon.hpp"
--- 3-Inheritance/3-Inheritance/iPolygon.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  iPolygon.cpp
-//  3-Inheritance
-//
-//  Created by Adam Saher on 2021-03-14.
-//
-
-#include "iPolygon.hpp"
