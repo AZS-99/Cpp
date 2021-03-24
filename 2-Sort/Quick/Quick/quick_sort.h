e3efe628ba390622664ce754e34f2bc743eb48b4
Adam <adam@Adams-MacBook-Pro.local>
2021-03-23 20:34:49 -0400

    merge

--- 2-Sort/Quick/Quick/quick_sort.h
+++ 2-Sort/Quick/Quick/quick_sort.h
@@ -16,21 +16,22 @@ void quick_sort(T* array, unsigned size) {
         T* left = array;
         T* right = array + size - 1;
         
-        
+        //Swap values greater than pivot on the left, for values less than pivot on the right
+        bool both_sides_unorganized = true;
         do {
             while(*left < *pivot) ++left;
             while(*right > *pivot) --right;
             
             if (left < pivot && right > pivot)
                 std::swap(*left, *right);
-        } while (left < pivot && right > pivot);
+            else
+                both_sides_unorganized = false;
+        } while (both_sides_unorganized);
         
         T* original_pivot_value_ptr = nullptr;
-        
+        // If there are values left on the left side, swap them accordingly, keeping track of the pivot's value
         if (left < pivot) {
-            while (left < pivot && *left < *pivot) ++left;
             original_pivot_value_ptr = left;
-            
             do {
                 std::swap(*left++, *pivot--);
                 while(*pivot > *original_pivot_value_ptr) --pivot;
@@ -39,9 +40,7 @@ void quick_sort(T* array, unsigned size) {
             std::swap(*pivot, *original_pivot_value_ptr);
             
         } else if (right > pivot) {
-            while (right > pivot && *right > *pivot) --right;
             original_pivot_value_ptr = right;
-            
             do {
                 std::swap(*right--, *pivot++);
                 while (*pivot < *original_pivot_value_ptr) ++pivot;
